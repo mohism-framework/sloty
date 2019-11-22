33afab52c3da94cbeeb3140bba6220c5c7765c67
蓝 <lanhao@class100.com>
2019-11-22 17:32:09 +0800

    fix: 🐛 generate Action , fix name unified

--- src/bin/gen.ts
+++ src/bin/gen.ts
@@ -4,6 +4,14 @@ import { readFileSync, existsSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 import Question from '../libs/question.class';
 
+const unifiedName = (name: string): string => {
+  const result: Array<string> = [];
+  name.split(/-|_/).forEach((word: string) => {
+    result.push(`${word[0].toUpperCase()}${word.substr(1)}`);
+  });
+  return result.join('');
+};
+
 const EXTS: Array<string> = [
   'ts',
   'js',
@@ -12,7 +20,7 @@ const EXTS: Array<string> = [
 (async () => {
   const name: string = await Question.input('input name:', 'foo');
   const ext: number = await Question.select('use Javascript or Typescript:', EXTS);
-  
+
   const content = readFileSync(resolve(`${__dirname}/../../gen_tpl/action.${EXTS[ext]}.tpl`)).toString();
   let outDir = './';
   if (existsSync('./src')) {
@@ -24,7 +32,7 @@ const EXTS: Array<string> = [
   const outFile = resolve(`${process.cwd()}/${outDir}/${name.toLowerCase()}.action.${EXTS[ext]}`);
   writeFileSync(
     outFile,
-    content.replace(/_FOO_/g, `${name[0].toUpperCase()}${name.substr(1)}`)
+    content.replace(/_FOO_/g, unifiedName(name)),
   );
   return outFile;
 })().then((v) => {
